0fbab1904e6ab761785035a34fd2b4a38dec0c98
Viviane Pons <viviane.pons@lri.fr>
2020-03-30 18:41:03 +0200

    Travis build scripts

---
+++ .travis-install.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved.  This file is offered as-is,
+# without any warranty.
+set -e
+cd $HOME
+if [ ! -x SageMath/sage ] ; then 
+    rm -f SageMath.tar.bz2
+    wget --progress=dot:giga $SAGE_SERVER$SAGE_IMAGE -O SageMath.tar.bz2
+    tar xf SageMath.tar.bz2
+fi
+MAKE="make -j4"
+export MAKE
+# Install packages
+#SageMath/sage -i lrslib
+# To initialize matplotlib font manager
+SageMath/sage -python -c 'import matplotlib.pyplot'
---
+++ .travis-test.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved.  This file is offered as-is,
+# without any warranty.
+set -e
+$HOME/SageMath/sage -pip install --upgrade --no-index -v .
+$HOME/SageMath/sage setup.py test
+# (cd docs && $HOME/SageMath/sage -sh -c "make html")
+$HOME/SageMath/sage -pip uninstall .
